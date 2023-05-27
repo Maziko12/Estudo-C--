87ed2dd00244878e41adc31885327fe58699e0ed
maKizo009 <113452307+maKizo009@users.noreply.github.com>
2023-05-27 16:27:02 -0300

    Create ex001.cpp
    Primeiro exercício (feito pelo notebook).

---
+++ ex001.cpp
@@ -0,0 +1,15 @@
+#include <ionstream>
+
+int main()
+{
+  std::cout << "KKKKK QUE BOBÃO KKKK" std::endl;
+  std::cout << "KKKKK GORDO" std::endl;
+  return 0;
+}
+
+// NÃO SEI O QUE FAZER
+{
+  std::cout << "KKKKKKKKKKK\n";
+  std::cout << "QU BURROO KKKKKK\n";
+  return 0;
+}
