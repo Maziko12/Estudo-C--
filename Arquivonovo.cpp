1ed4c11d1bcc6cc2b9a1a76ac5ee723479b1dc46
Maziko12 <128994920+Maziko12@users.noreply.github.com>
2023-06-10 01:55:17 -0300

    n bhy

--- Arquivonovo.cpp
+++ Arquivonovo.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-  std::cout << "Olá! Me diga quantos anos você tem colocando a quantidade. De mês depois de um ponto para"; cout << "tornar esse número float, que eu te mostrarei ele convertido em um número inteiro!";
+  cout << "Olá! Me diga quantos anos você tem colocando a quantidade. De mês depois de um ponto para tornar esse número float, que eu te mostrarei ele convertido em um número inteiro!";
+  
   float Idade;
   cout << "\nR: ";
+  cin >> Idade;
   
   cout << Idade;
 
---
+++ aa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void Ola()
+{
+  // a função irá criar uma função que é essa, que vai pedir ao usuário digitar "Olá", e então vai imprimir isso.    
+  string Hello;
+  cout << "Digite você também! 'Olá'.\nR: ";
+  cin >> Hello;
+    
+  cout << "Muito bem!!!";
+}
+
+int main ()
+{
+  cout << "Olá!";
+  Ola();
+  return 0;
+}
