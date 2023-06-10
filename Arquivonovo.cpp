d7500dc29a0ff4d3c415505c1f9f70cdc9abc923
Maziko12 <128994920+Maziko12@users.noreply.github.com>
2023-06-09 23:56:29 -0300

    kkkk

---
+++ Arquivonovo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  std::cout << "Olá! Me diga quantos anos você tem colocando a quantidade. De mês depois de um ponto para"; cout << "tornar esse número float, que eu te mostrarei ele convertido em um número inteiro!";
+  float Idade;
+  cout << "\nR: ";
+  
+  cout << Idade;
+
+  return 0;
+}
