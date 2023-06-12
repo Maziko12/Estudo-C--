a4b3e144102fede907014f9084834fe591a447b7
Maziko12 <128994920+Maziko12@users.noreply.github.com>
2023-06-11 22:27:44 -0300

    kkkkkk

---
+++ ExerCíCiosDeC++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/*Escreva uma função chamada soma que recebe dois números inteiros como parâmetros e retorna a soma dos dois números.
+
+Escreva uma função chamada ehPar que recebe um número inteiro como parâmetro e retorna true se o número for par e false caso contrário.
+
+Escreva uma função chamada calcularMedia que recebe um vetor de números inteiros como parâmetro e retorna a média dos valores no vetor.
+
+Escreva uma função chamada inverterString que recebe uma string como parâmetro e retorna uma nova string com os caracteres invertidos.
+
+Escreva uma função chamada fatorial que recebe um número inteiro como parâmetro e retorna o fatorial desse número.
+
+Escreva uma função chamada contarVogais que recebe uma string como parâmetro e retorna a quantidade de vogais presentes na string.
+
+Escreva uma função chamada ehPrimo que recebe um número inteiro como parâmetro e retorna true se o número for primo e false caso contrário.
+
+Escreva uma função chamada calcularPotencia que recebe dois números inteiros como parâmetros, base e expoente, e retorna o valor da base elevada ao expoente.
+
+Escreva uma função chamada concatenarStrings que recebe duas strings como parâmetros e retorna uma nova string que contém a concatenação das duas strings.
+
+Escreva uma função chamada encontrarMaior que recebe um vetor de números inteiros como parâmetro e retorna o maior valor presente no vetor.*/
+
---
+++ ex002.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*Escreva uma função chamada soma que recebe dois números inteiros como parâmetros e retorna a soma dos dois números.*/
+
+int soma(int num, int num2)
+{
+  return num + num2;
+}
+int main ()
+{
+  cout << "Olá, como vai? Seja bem-vindo(a) ao meu programa!";
+  cout << "---------------_---------------\n" << endl;
+
+  int num;
+  cout << endl << "\nVocê poderia me dizer dois números? Eu te direi a soma deles!\nPrimeiro número: ";
+  cin >> num;
+
+  int num2;
+  cout << "Ok! Agora poderia me dizer mais um número?\nO anterior foi uma ótima escolha!\n\nSegundo número: ";
+  cin >> num2;
+
+  int resultado = soma(num, num2);
+  
+  cout << "O valor da soma deu: " << resultado << endl;
+  cout << "Obrigado! Até uma próxima vez!" << endl;
+  return 0;
+}
