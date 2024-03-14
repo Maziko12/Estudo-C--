e072ebd46ee47aaa60c4c3ada11a54b3e89e4551
Maziko12 <128994920+Maziko12@users.noreply.github.com>
2024-03-14 02:57:39 -0300

    Update aa.cpp

--- aa.cpp
+++ aa.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void Ola()
 {
   // a função irá criar uma função que é essa, que vai pedir ao usuário digitar "Olá", e então vai imprimir isso.    
-  string Hello;
+  std::string Hello;
   cout << "Digite você também! 'Olá'.\nR: ";
   cin >> Hello;
     
